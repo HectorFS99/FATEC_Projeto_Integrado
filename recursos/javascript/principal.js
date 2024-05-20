14872030f325f779973bb591596baa0ae5959b93
Hector Ferreira da Silva <fhector7@gmail.com>
2024-05-20 15:32:24 -0300

    Ajustes para eliminação de delay, ao carregar estilos da página; Centralização da criação da tag <header>, para evitar repetição de código e reaproveitar o mesmo código em todas as páginas.

---
+++ recursos/javascript/principal.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Monta a tag <header> em todas as páginas, a fim de evitar repetição de código em cada uma delas.
+    // *** Todas as páginas terão a mesma barra de navegação superior.
+    
+    document.body.insertAdjacentHTML('afterbegin', 
+        `<header>
+            <nav class="navbar">
+                <img src="recursos/imagens/logo_fatec_cor.png" width="125">
+            </nav>    
+        </header>`
+    );
+});
