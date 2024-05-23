d12bee75544e6f46f12851ac04d4bad1bafbd325
Hector Silva <fhector7@gmail.com>
2024-05-23 00:40:09 -0300

    CADASTRO: Validação do nome.

--- recursos/javascript/principal.js
+++ recursos/javascript/principal.js
@@ -7,7 +7,47 @@ document.addEventListener("DOMContentLoaded", function() {
         `<header class="fixed-top">
             <nav class="navbar">
                 <img src="recursos/imagens/logo_fatec_cor.png" width="125">
-            </nav>    
+            </nav>
         </header>`
     );
 });
+
+function validarCampo(id_campo, id_feedback) {
+    var campo = document.getElementById(id_campo);
+    var div_feedback = document.getElementById(id_feedback);
+
+    switch (id_campo) {
+        case 'txtNome':
+            if (campo.value.length < 2) {
+                exibirFeedback(campo, div_feedback, 'Nome inválido. Informe-o corretamente.')
+            } else if (validarCaracteresEspeciais(campo.value)) {
+                exibirFeedback(campo, div_feedback, 'Não são permitidos caracteres especiais. Informe um nome válido.');
+                campo.value = '';
+            } else {
+                limparFeedback(div_feedback);
+            }
+
+            break;
+        case 'dtNasc':
+
+            break;
+    }
+
+    return;
+}
+
+function validarCaracteresEspeciais(texto) {
+    return /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(texto);
+}
+
+function exibirFeedback(campo, div_feedback, mensagem) {
+    div_feedback.style.display = 'block';
+    div_feedback.innerHTML = `<strong>${mensagem}</strong>`;
+
+    campo.focus();
+}
+
+function limparFeedback(div_feedback) {
+    div_feedback.innerHTML = '';
+    div_feedback.style.display = 'none';
+}
