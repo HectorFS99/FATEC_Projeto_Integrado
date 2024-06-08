5589e04e289d86ed6ddf1cf5d1a1892029f021c9
Hector Silva <fhector7@gmail.com>
2024-06-08 01:23:20 -0300

    Criação do componente de frete e escolha de opç~eos de entrega na página de pagamento.

--- recursos/javascript/pagamento.js
+++ recursos/javascript/pagamento.js
@@ -1,5 +1,7 @@
-function exibirAccordion(id_accordion) {
-    listaAccordions = document.getElementsByClassName('container-accordion');
+function exibirAccordion(id_accordion, id_componente) {
+    let componente = document.getElementById(id_componente);
+    let listaAccordions = componente.getElementsByClassName('container-accordion');
+    
     for (let i = 0; i < listaAccordions.length; i++) {
         listaAccordions[i].style.display = "none";
     }
@@ -7,6 +9,7 @@ function exibirAccordion(id_accordion) {
     accordion = document.getElementById(id_accordion);
     accordion.style.display = "block";
 }
+
 
 function aplicarMascaraNumeroCartao(campo) {
     campo.value = campo.value.replace(/[^0-9 ]/g, ''); // Remove letras e mantém apenas números, ponto (.) e hífen (-).
