d81d70f5e74b7cb09378e099cf806333e0bc0aeb
Hector Silva <fhector7@gmail.com>
2024-09-16 19:48:29 -0300

    Scripts das tabelas de "avaliacoes_curtidas" e "avaliacoes"

---
+++ banco_dados/16-09-24/avaliacoes.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.2
+-- http://www.phpmyadmin.net
+--
+-- Máquina: localhost
+-- Data de Criação: 16-Set-2024 às 22:47
+-- Versão do servidor: 5.6.13
+-- versão do PHP: 5.4.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de Dados: `future_mob`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `avaliacoes`
+--
+
+CREATE TABLE IF NOT EXISTS `avaliacoes` (
+  `id_avaliacao` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `avaliacao` decimal(2,1) NOT NULL,
+  `dt_avaliacao` date NOT NULL,
+  `titulo` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `descricao` varchar(1000) CHARACTER SET utf8 DEFAULT NULL,
+  `imagem` varchar(255) DEFAULT NULL,
+  `verificado` bit(1) NOT NULL,
+  PRIMARY KEY (`id_avaliacao`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ banco_dados/16-09-24/avaliacoes_curtidas.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.2
+-- http://www.phpmyadmin.net
+--
+-- Máquina: localhost
+-- Data de Criação: 16-Set-2024 às 22:46
+-- Versão do servidor: 5.6.13
+-- versão do PHP: 5.4.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de Dados: `future_mob`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `avaliacoes_curtidas`
+--
+
+CREATE TABLE IF NOT EXISTS `avaliacoes_curtidas` (
+  `id_cliente` int(11) NOT NULL,
+  `id_avaliacao` int(11) NOT NULL,
+  `curtiu` bit(1) NOT NULL,
+  `descurtiu` bit(1) NOT NULL,
+  `dt_interacao` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
