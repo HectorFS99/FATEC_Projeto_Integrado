bb1d108c37d9596429fe0e982464b256d2a9aec4
EllenEufrasio <elleneufrasio09@gmail.com>
2024-09-22 09:31:06 -0300

    Configurações de linguagem UTF-8

--- admin/acoes_php/categoria/excluir-categoria.php
+++ admin/acoes_php/categoria/excluir-categoria.php
@@ -2,7 +2,6 @@
    	header('Content-Type: text/html; charset=utf-8');
     include '../conectar_banco_dados.php';     
     
-    
     if(isset($_GET['APAGAR'])){
         $sql = mysql_query ("DELETE FROM categorias
                              WHERE nome =". $_GET['APAGAR']);
