31530ccb6966c89902f29d9171307018541a0b22
EllenEufrasio <elleneufrasio09@gmail.com>
2024-09-22 09:25:08 -0300

    Atualização página de excluir categoria

--- admin/acoes_php/categoria/excluir-categoria.php
+++ admin/acoes_php/categoria/excluir-categoria.php
@@ -1,4 +1,28 @@
 <?php
    	header('Content-Type: text/html; charset=utf-8');
-    include '../conectar_banco_dados.php';        
+    include '../conectar_banco_dados.php';     
+    
+    
+    if(isset($_GET['APAGAR'])){
+        $sql = mysql_query ("DELETE FROM categorias
+                             WHERE nome =". $_GET['APAGAR']);
+        echo "
+            <script>
+                Swal.fire({
+                    title: 'Categoria Excluída',
+                    icon: 'success'
+                });
+            </script>
+        ";
+        } else {
+        echo "
+            <script>
+                Swal.fire({
+                    title: 'Erro ao excluir categoria',
+                    icon: 'error'
+                });
+            </script>
+        ";
+    }
+
 ?>
