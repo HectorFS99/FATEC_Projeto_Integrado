6c1d220c407cc0b5d14811982f7a1b9cf34ee2f0
Enzo Ribeiro <160975894+EnzoRibeir@users.noreply.github.com>
2024-09-22 23:28:40 -0300

    forçando conexao com banco a utilizar utf8, debugando a nao identificação de palavras com acento agudo no codigo

--- conexao.php
+++ conexao.php
@@ -6,4 +6,9 @@
 
     $conecta_db = mysql_connect($servidor, $usuario, $senha) or die (mysql_error());
     mysql_select_db($banco) or die ("Erro ao conectar ao banco de dados.");
+
+    mysql_query("SET NAMES 'utf8'");
+    mysql_query("SET character_set_connection=utf8");
+    mysql_query("SET character_set_client=utf8");
+    mysql_query("SET character_set_results=utf8");
 ?>
