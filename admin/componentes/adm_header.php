3bcab316ed64d93cb78b6bc4c64880b816196a0f
Hector Silva <fhector7@gmail.com>
2024-11-18 15:20:39 -0300

    atualizacão banco para inclusão de coluna de imagem de perfil e dinamização do usuário logado na área administrativa.

--- admin/componentes/adm_header.php
+++ admin/componentes/adm_header.php
@@ -1,3 +1,37 @@
+<?php
+    session_start();
+    date_default_timezone_set('America/Sao_Paulo'); // Define o fuso horário para o Brasil
+
+	include '../admin/acoes_php/conectar_banco_dados.php';
+
+    if (!isset($_SESSION['id_usuario'])) {
+        echo 
+            "<script>
+                alert('Usuário não autenticado. Você será redirecionado para a página inicial.')
+                window.location.href = '../pagina-inicial.php';
+            </script>";        
+    }
+
+    $id_usuario = $_SESSION['id_usuario'];
+
+    $sql_usuario = mysql_query(
+        "SELECT 
+            nome_completo
+            , telefone_celular
+            , email
+            , caminho_img_perfil
+        FROM
+            usuarios
+        WHERE 
+            id_usuario = $id_usuario"
+    ) or die("Erro ao obter dados do usuário: " . mysql_error());
+
+    $usuario = mysql_fetch_assoc($sql_usuario);
+    if (!$usuario) {
+        die("Usuário não encontrado.");
+    }
+?>
+
 <header class="cabecalho">
     <div>
         <h1>Painel Administrativo</h1>
@@ -5,8 +39,8 @@
     </div>
     <div class="dropdown">
         <button class="botao btn-usuario" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <span class="mx-2">Usuário Admin.</span>
-            <img src="recursos/imagens/usuarios/admin.png">
+            <span class="mx-2"><?php echo $usuario['nome_completo']; ?></span>
+            <img src="<?php echo $usuario['caminho_img_perfil']; ?>">
         </button>
         <ul class="dropdown-menu">
             <li>
