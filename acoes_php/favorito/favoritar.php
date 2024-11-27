390a48b8074c7c3cd5198a89323fe7e595447774
Hector Ferreira da Silva <fhector7@gmail.com>
2024-11-27 02:00:03 -0300

    finalização dos favoritos

--- acoes_php/favorito/favoritar.php
+++ acoes_php/favorito/favoritar.php
@@ -7,49 +7,39 @@
     $id_produto = $_GET['id_produto'];
     $id_usuario = $_SESSION['id_usuario'];
 
-    $sql = mysql_query(
-        "INSERT INTO favoritos(
-            id_usuario
-            , id_produto
-        ) VALUES (
-            $id_usuario 
-            , $id_produto
-        )"
-    );
+	// Verifica se o produto já existe nos favoritos para este usuário.
+	$sql_check = mysql_query(
+		"SELECT id_produto FROM favoritos WHERE id_produto = $id_produto AND id_usuario = $id_usuario"
+	);
 
-    if ($sql == 1) {
+	if (mysql_num_rows($sql_check) == 0) {
+		// Produto não existe nos favoritos, insere um novo registro.
+        $sql = mysql_query(
+            "INSERT INTO favoritos(
+                id_usuario
+                , id_produto
+            ) VALUES (
+                $id_usuario 
+                , $id_produto
+            )"
+        );
+    
+        if ($sql == 1) {
+            echo
+                "<script>
+                    window.location.href = '../../favoritos.php';
+                </script>";
+        } else {
+            echo
+                "<script>
+                    notificar(true, 'Erro ao favoritar item.', '', mysql_error(), '../../listagem-geral-produtos.php')
+                    alert(mysql_error());
+                </script>";
+        }
+	} else {
         echo
             "<script>
                 window.location.href = '../../favoritos.php';
             </script>";
-    } else {
-        echo
-            "<script>
-                notificar(true, 'Erro ao cadastrar endereço', '', mysql_error(), '../../listagem-geral-produtos.php')
-                alert(mysql_error());
-            </script>";
     }
-
-    $result = mysql_query( 
-        "SELECT 
-            f.id_produto
-            , p.nome
-            , p.caminho_imagem
-            , p.preco_atual
-            , p.preco_anterior
-        FROM 
-            favoritos AS f
-            INNER JOIN produtos AS p ON f.id_produto = p.id_produto
-        WHERE 
-            f.id_usuario = $id_usuario"
-    );    
-
-    // Array para armazenar os itens favoritos
-    $itens_favoritos = [];
-    if (mysql_num_rows($result) > 0) {
-        while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-            $itens_favoritos[] = $row;
-        }
-    }
-
 ?>
--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <?php
     header('Content-Type: text/html; charset=utf-8');
+    date_default_timezone_set('America/Sao_Paulo');
+
     include 'conexao.php';
     session_start();
     
